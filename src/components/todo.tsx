3bfd968bd5e1bd309289263c1f97bc514b783011
Nate Fleming <Nate@Nates-MBP.attlocal.net>
2020-05-17 19:28:32 -0500

    refactored update functionality, made each row 100% width

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,56 +1,92 @@
-import React, { useState } from 'react';
-import { ToDo } from './todo-list';
-import { Popover, Position, Menu, Text, TextInput, Button, Badge, Pane, Table } from 'evergreen-ui';
-
+import React, { useState } from "react";
+import { ToDo } from "./todo-list";
+import {
+  Popover,
+  Position,
+  Menu,
+  Text,
+  TextInput,
+  Button,
+  Badge,
+  Pane,
+  Table,
+} from "evergreen-ui";
 
 interface IProps {
-    todo: ToDo;
-    index: number;
-    onDelete(index:number): void;
-    onEdit(index:number, title:string): void;
+  todo: ToDo;
+  index: number;
+  onDelete(index: number): void;
+  onEdit(index: number, title: string): void;
 }
 
-const ToDoListItem: React.FC<IProps> = ({todo,index, onDelete, onEdit}) => {
-    const [isEditable, setIsEditable] = useState(false);
-    const [title, setNewTitle] = useState(todo.title);
-    const [isCompleted, toggleCompleted] = useState(todo.isCompleted)
-    
-    return (
-        <Table.Row key={index} isSelectable>
-            <Pane verticalAlign='center' marginLeft={16}>
-        { isEditable ?
-        <> 
-        <TextInput
-            name="add-todo"
-            placeholder={todo.title}
-            onChange={(e: any) => setNewTitle(e.target.value)}
-            value={title}
-          /> 
-          <Button onClick={() => {onEdit(index, title); setIsEditable(false)}}>Edit</Button>
+const ToDoListItem: React.FC<IProps> = ({ todo, index, onDelete, onEdit }) => {
+  const [isEditable, setIsEditable] = useState(false);
+  const [title, setNewTitle] = useState(todo.title);
+  const [isCompleted, toggleCompleted] = useState(todo.isCompleted);
+
+  return (
+    <Table.Row key={index} isSelectable>
+      <Pane display="flex" alignItems="center" marginLeft={16} width="100%">
+        {isEditable ? (
+          <>
+            <TextInput
+              width="auto"
+              name="add-todo"
+              placeholder={title}
+              onChange={(e: any) => setNewTitle(e.target.value)}
+              value={title}
+            />
+            <Button
+              marginLeft={16}
+              onClick={() => {
+                setIsEditable(false);
+              }}
+            >
+              Edit
+            </Button>
           </>
-          :
-        <Popover
-        position={Position.BOTTOM_LEFT}
-        content={
-          <Menu>
-              <Menu.Item icon="edit" onSelect={() => toggleCompleted(!isCompleted)} >
-                Complete...
-              </Menu.Item>
-              <Menu.Item icon="edit" onSelect={() => setIsEditable(true)} >
-                Edit...
-              </Menu.Item>
-              <Menu.Item icon="trash" intent="danger" onSelect={() => onDelete(index)}>
-                Delete...
-              </Menu.Item>
-          </Menu>
-        }
-        >
-           <Text  width='100%'>{title} {isCompleted && <Badge marginLeft='auto' marginRight={16} color="green">Completed</Badge>}</Text>
-        </Popover>
-    }
-    </Pane>
+        ) : (
+          <Popover
+            position={Position.BOTTOM_LEFT}
+            content={
+              <Menu>
+                <Menu.Item
+                  icon="cross"
+                  onSelect={() => toggleCompleted(!isCompleted)}
+                >
+                  Complete...
+                </Menu.Item>
+                <Menu.Item icon="edit" onSelect={() => setIsEditable(true)}>
+                  Edit...
+                </Menu.Item>
+                <Menu.Item
+                  icon="trash"
+                  intent="danger"
+                  onSelect={() => onDelete(index)}
+                >
+                  Delete...
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            <Pane
+              width="100%"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text>{title}</Text>
+              {isCompleted && (
+                <Badge marginLeft="auto" marginRight={16} color="green">
+                  Completed
+                </Badge>
+              )}
+            </Pane>
+          </Popover>
+        )}
+      </Pane>
     </Table.Row>
-    )
-}
+  );
+};
 
 export default ToDoListItem;
